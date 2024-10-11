5bd4e7f3a0676acfd965e2fe7885c5811558b1ef
MicheleTosi <micheletosi98@gmail.com>
2024-10-12 01:12:34 +0200

    modificata struttura progetto

--- utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UTILS_H
-#define _UTILS_H
-
-extern char *get_absolute_path(const char *filename);
-extern char *get_dir_parent(char *path);
-extern char* get_cwd(void);
-extern int temp_file(char *name);
-
-#endif
